35d2caaadd08c008d62499472e520ace78ffda99
jorgep <jpaez@us.ibm.com>
2016-02-08 14:41:31 -0600

    step 5 - setup http GET and test for it

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,21 +1,41 @@
 'use strict';
 
-// append the phoneListCtrl to my tutorial app to the
-tutorialApp.controller('PhoneListCtrl', function($scope) {
-  // append phones attribute to the scope local to the phoneListCtrl
-  // Note: this scope inherits prototypically the attributes of the 
-  //       parent scope. 
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age': 1},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 2},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 3 }
-  ];
+// append the phoneListCtrl to my tutorial app. We are using an annotation 
+// for the injections so that angular's injector can pick up depencies even 
+// when the code has been minified.
+tutorialApp.controller('PhoneListCtrl', ['$scope', '$http', 
+  function PhoneListCtrl($scope, $http) {
+    // append phones attribute to the scope local to the phoneListCtrl
+    // Note: this scope inherits prototypically the attributes of the 
+    //       parent scope. 
+    // Commentted out static hard-coded data
+    // $scope.phones = [
+    //   {'name': 'Nexus S',
+    //    'snippet': 'Fast just got faster with Nexus S.',
+    //    'age': 1},
+    //   {'name': 'Motorola XOOM™ with Wi-Fi',
+    //    'snippet': 'The Next, Next Generation tablet.',
+    //    'age': 2},
+    //   {'name': 'MOTOROLA XOOM™',
+    //    'snippet': 'The Next, Next Generation tablet.',
+    //    'age': 3 }
+    // ];
 
-  $scope.orderProp = 'age';
-});
+
+    // Simple GET AJAX request to get phones data from the server
+    // 
+    $http.get('phones/phones.json')
+        .success(function(data) { 
+            $scope.phones = data; 
+            // show just a segment of the data received
+            // $scope.phones = data.splice(0, 5);
+        });
+
+
+
+    $scope.orderProp = 'age';
+  }]);
+
+// Other way of achieiving the injection beyond minification is:
+// PhoneListCtrl.$inject = ['$scope', '$http']
+// tutorialApp.controller('PhoneListCtrl', PhoneListCtrl);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,10 +3,33 @@
 /* jasmine specs for controllers go here */
 // This unit test are written with the Jasmine API and are run using Karma
 describe('AngularTutorialApp', function() {
-  var scope, ctrl;
+  var scope, ctrl,
+      $httpBackend; // In order to test $http requests
   
   // before each test create our app module
   beforeEach(module('AngularTutorialApp'));
+
+  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+  // This allows us to inject a service but then attach it to a variable
+  // with the same name as the service in order to avoid a name conflict.
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    // Mock angular services come from angular-mocks.js in the karma config.
+
+    // Setup mock $http
+    $httpBackend = _$httpBackend_;
+    //  In tutorial this line was $httpBackend.expectGET('phones/phones.json')
+    // but that was giving an error or request when unexpected i think because 
+    // the beforeEach was being called multiple times but this feel a bit wear
+    // with .when() test passes but i might need further research
+    $httpBackend.when('GET', 'phones/phones.json').
+        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+    // testing scope
+    scope = $rootScope.$new();
+
+
+    ctrl = $controller('PhoneListCtrl', {$scope: scope });
+  }));
 
   // inject will pass to the test logic the $controller which allows us to lookup
   // the controller to be tested by name. In this lookup we can pass mock dependencies
@@ -16,11 +39,15 @@ describe('AngularTutorialApp', function() {
     ctrl = $controller('PhoneListCtrl', {$scope:scope});
   }));
 
-  it('should create "phones" model with 3 phones', function() {
-      expect(scope.phones.length).toBe(3);
-    });
-
   it('should set the default value of orderProp model', function() {
     expect(scope.orderProp).toBe('age');
+  });
+
+  it('should create "phones" model with 2 phones fetched from xhr', function() {
+    expect(scope.phones).toBeUndefined();
+    $httpBackend.flush();
+
+    expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                 {name: 'Motorola DROID'}]);
   });
 });
